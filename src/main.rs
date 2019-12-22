87c9054415203425739c5cd4d519a7080d24e26a
Matt Weller <hellmitre@gmail.com>
2019-12-22 12:05:22 -0800

    frmt

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,10 @@ fn ensure_config_dir() {
   let mut _a = match conf_dir {
     Some(p) => {
       match fs::create_dir_all(p.join(Path::new("homemaker"))) {
-        Ok(r) => r,
+        Ok(r) => r, // if no problem, return the thing in the thing
         Err(err) => panic!("Couldn't create {}. Error: {}", p.display(), err.to_string()),
       };
     },
-    None => panic!("No $HOME/.config found!"),
+    None => panic!("No $HOME/.config found!"), // if dirs:: call doesn't return anything
   };
 }
