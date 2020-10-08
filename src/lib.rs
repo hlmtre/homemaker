4d3df2dff02a83aacaae54ba3acd03d628e899d6
Matt Weller <hellmitre@gmail.com>
2020-10-08 16:33:32 -0700

    further cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,66 @@
-//! hm provides a library to do basic filesystem operations that you'd want a dotfile
-//! manager to do (like symlink). Also provides functionality to thread off heavier
-//! operations into task threads, which will regularly report back their status with
-//! Worker objects over a std::sync::mpsc.
+//! `hm` (short for `homemaker`) provides a library to do basic filesystem operations
+//! that you'd want a dotfile manager to do (like symlink).
+//! It also provides functionality to thread off heavier operations
+//! into task threads, which regularly report back their status with
+//! Worker objects over a `std::sync::mpsc`.
+//!
+//! `hm` can also perform dependency resolution, thanks to the solvent crate. You can
+//! provide a big ol' list of tasks to complete, each with their own dependencies, and
+//! as long as you have a solveable dependency graph, you can get workable batches from
+//! get_task_batches(). They will be in *some* order that will resolve your dependencies,
+//! but that order is non-deterministic - if there's multiple ways to solve a graph,
+//! you'll probably get all those different ways back if you run it multiple times.
+//!
+//! The crate provides this library, which is in turn used by the bin `hm` (in `src/bin/main.rs`).
+//! `hm` is a commandline program to help with dotfile (and more) management.
+//!
+//! It can handle putting configuration files where they should go, but its real
+//! strength lies in the solutions it'll execute - shell scripts, usually - to
+//! pull a git repository, compile something from source, etc.
+//!
+//! `hm` exists because I bring along a few utilities to all Linux boxes I regularly
+//! use, and those are often built from source. So rather than manually install
+//! all dependency libraries, then build each dependent piece, then finally the
+//! top-level dependent program, I built hm.
+//!
+//!
+//! [gfycat of it in action](https://gfycat.com/skinnywarmheartedafricanpiedkingfisher)
+//!
+//!
+//!  1. create a config.toml file either anywhere or in ~/.config/homemaker/.
+//!  2. enter things to do things to in the file.
+//!  example:
+//!  ``` text
+//!  ## config.toml
+//!
+//!  [[obj]]
+//!  file = 'tmux.conf'
+//!  source = '~/dotfiles/.tmux.conf'
+//!  destination = '~/.tmux.conf'
+//!  method = 'symlink'
+//!
+//!  [[obj]]
+//!  task = 'zt'
+//!  solution = 'cd ~/dotfiles/zt && git pull'
+//!  dependencies = 'maim, slop'
+//!
+//!  [[obj]]
+//!  task = 'slop'
+//!  source = '~/dotfiles/zt/slop'
+//!  solution = 'cd ~/dotfiles/zt/slop; make clean; cmake -DCMAKE_INSTALL_PREFIX="/usr" ./ && make && sudo make install'
+//!  method = 'execute'
+//!  ```
+//!  3. `hm ~/path/to/your/config.toml`
+//!
+//!  [![built with spacemacs](https://cdn.rawgit.com/syl20bnr/spacemacs/442d025779da2f62fc86c2082703697714db6514/assets/spacemacs-badge.svg)](http://spacemacs.org)
+//!
+//!  thanks to actual good code:
+//!  serde
+//!  toml
+//!  symlink
+//!  solvent
+//!  indicatif
+//!  console
 extern crate console;
 extern crate indicatif;
 extern crate shellexpand;
