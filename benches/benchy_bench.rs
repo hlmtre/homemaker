de890e1e8dde473c118e2d46139b256c05d62dda
Matt Weller <hellmitre@gmail.com>
2020-12-29 23:08:34 -0800

    typo

--- benches/benchy_bench.rs
+++ benches/benchy_bench.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use hm::{
   config::{deserialize_file, Config, ManagedObject},
   get_task_batches,
@@ -8,7 +8,7 @@ use std::collections::HashMap;
 fn criterion_benchmark(c: &mut Criterion) {
   let a: Config = deserialize_file("./src/config.toml").unwrap();
   let nodes: HashMap<String, ManagedObject> = Config::as_managed_objects(a);
-  c.bench_function("get_task_bathces", |b| {
+  c.bench_function("get_task_batches", |b| {
     b.iter(|| get_task_batches(nodes.clone()))
   });
 }
